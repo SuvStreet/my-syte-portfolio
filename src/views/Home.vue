1c5014c355c6ad84267fa65d3f99ac245d7f8ef5
Dmitrij Kolotusha <suvstreet@gmail.com>
2022-02-13 20:45:53 +0100

    animation text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
             Привет, меня зовут <span class="name">Дмитрий Колотуша</span>
           </h3>
           <h3 class="my-profession">
-            Я <span class="typing">веб-разработчик</span>
+            Я <span class="typing"></span>
           </h3>
           <p>
             Веб-разработчик с годовым опытом работы. Опыт заключается в создании
@@ -27,14 +27,33 @@
 </template>
 
 <script>
-export default {}
+import Typed from 'typed.js'
+import { onMounted } from 'vue'
+
+export default {
+  setup() {
+    onMounted(() => {
+      textAnimation()
+    })
+
+    function textAnimation() {
+      new Typed('.typing', {
+        strings: [
+          '',
+          'веб разработчик',
+          'верстаю сайты',
+          'junior frontend разработчик',
+          'js специалист',
+        ],
+        typeSpeed: 100,
+        BackSpeed: 60,
+        loop: true,
+      })
+    }
+    return {}
+  },
+  components: {},
+}
 </script>
 
-<style lang="sass">
-.v-enter-active,.v-leave-active
-  transform: translateX(1000px)
-  transition: all 0.5s ease
-
-.v-enter-from,.v-leave-to
-  transform: translateX(0px)
-</style>
+<style lang="sass"></style>
