2c51d726de8cba65b233866690b3f988100a5025
Dmitrij Kolotusha <suvstreet@gmail.com>
2022-07-27 22:43:58 +0200

    add title project portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -12,69 +12,40 @@
         </div>
       </div>
 
-      <TheSort/>
+      <TheSort />
 
       <div class="row">
-
-        <div class="portfolio-item padd-15">
+        <div
+          class="portfolio-item padd-15"
+          v-for="id in volumePortfolio"
+          :key="id"
+        >
           <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/movie?w=150&h=220" alt="poster">
+            <div class="portfolio-item-bg">
+              <div class="portfolio-img">
+                <img :src="picture" alt="poster" />
+              </div>
+              <h2 class="portfolio-title">Lorem, ipsum dolor.</h2>
             </div>
           </div>
         </div>
-
-        <div class="portfolio-item padd-15">
-          <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/game?w=150&h=220" alt="poster">
-            </div>
-          </div>
-        </div>
-
-        <div class="portfolio-item padd-15">
-          <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/album?w=150&h=220" alt="poster">
-            </div>
-          </div>
-        </div>
-
-        <div class="portfolio-item padd-15">
-          <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/book?w=150&h=220" alt="poster">
-            </div>
-          </div>
-        </div>
-
-        <div class="portfolio-item padd-15">
-          <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/fashion?w=150&h=220" alt="poster">
-            </div>
-          </div>
-        </div>
-
-        <div class="portfolio-item padd-15">
-          <div class="portfolio-item-inner shadow-dark">
-            <div class="portfolio-img">
-              <img src="https://api.lorem.space/image/watch?w=150&h=220" alt="poster">
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 import TheSort from '../components/TheSort.vue'
 
 export default {
   setup() {
+    const picture = ref('https://api.lorem.space/image/watch?w=150&h=220')
+    const volumePortfolio = ref(3)
+
     return {
+      picture,
+      volumePortfolio,
     }
   },
   components: {
