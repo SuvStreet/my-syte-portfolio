cf99fbdc8d1cf8524d81de263ea4f20e6ea6698a
Dmitrij Kolotusha <suvstreet@gmail.com>
2022-09-21 20:29:30 +0200

    add style in component experience

--- src/components/experience/ProgressBar.vue
+++ src/components/experience/ProgressBar.vue
@@ -12,13 +12,12 @@
       <div class="text">
         <h2>{{ percent }}%</h2>
         <small>
-          {{ technology }}
-          <span v-html="icons[technology]"></span>
+          <p>{{ technology }}</p>
+          <span class="icon" v-html="icons[technology]"></span>
         </small>
       </div>
       <div class="direction">
         <p>Lorem ipsum dolor sit amet.</p>
-
         <!-- <p>Работа с React</p> -->
       </div>
     </div>
@@ -47,71 +46,4 @@ export default {
 }
 </script>
 
-<style>
-.experience-item-inner {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.circle {
-  width: 200px;
-  height: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 40px;
-}
-
-.text {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  color: var(--text-black-700);
-}
-
-.text h2 {
-  font-size: 18px;
-  font-weight: 400;
-  letter-spacing: 1px;
-  color: var(--text-black-900);
-}
-
-.text small {
-  font-size: 18px;
-  display: block;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.circle .points {
-  width: 3px;
-  height: 15px;
-  background: #0007;
-  position: absolute;
-  border-radius: 3px;
-  transform: rotate(calc(var(--i) * var(--rot))) translateY(-100px);
-}
-
-.points.marked {
-  animation: glow 0.04s linear forwards;
-  animation-delay: calc(var(--i) * 0.05s);
-}
-
-@keyframes glow {
-  0% {
-    background: #0007;
-    box-shadow: none;
-  }
-
-  100% {
-    background: var(--skin-color);
-    box-shadow: 0 0 10px var(--skin-color);
-  }
-}
-</style>
+<style></style>
