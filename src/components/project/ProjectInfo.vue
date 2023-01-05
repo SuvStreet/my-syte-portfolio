a33798a39e6288b2a1fbb18c7ec1e8a475420f09
Dmitrij Kolotusha <suvstreet@gmail.com>
2023-01-05 20:02:08 +0300

    edited styles on the Portfolio page

--- src/components/project/ProjectInfo.vue
+++ src/components/project/ProjectInfo.vue
@@ -1,52 +1,51 @@
 <template>
-      <div
-        :class="['project-card', $store.getters.getIsToggleDetails]"
-        ref="card"
-      >
-        <div class="row">
-          <div class="project-tag padd-15">
-            <small v-for="(tag, id) in info.hash_tag" :key="id">
-              #{{ tag }}
-            </small>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="project-content padd-15">
-            <div class="project-title">
-              <h3>{{ $t('projectInfo.description') }}:</h3>
-              <p>
-                {{ info.description[$store.getters['i18n/getLanguage']] }}
-              </p>
-            </div>
-            <div class="project-tech-title">
-              <h3>{{ $t('projectInfo.info') }}:</h3>
-
-              <ul>
-                <li v-for="(dataProject, id) in info.info" :key="id">
-                  <strong>
-                    {{ dataProject.title[$store.getters['i18n/getLanguage']] }}
-                  </strong>
-                  -
-                  <a
-                    v-if="dataProject.description.indexOf('https') !== -1"
-                    :href="dataProject.description"
-                    target="_blank"
-                  >
-                    {{ dataProject.title[$store.getters['i18n/getLanguage']] }}
-                  </a>
-                  <template v-else>
-                    {{ dataProject.description }}
-                  </template>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr />
+  <div :class="['project-card', $store.getters.getIsToggleDetails]" ref="card">
+    <div class="row">
+      <div class="project-tag padd-15">
+        <small v-for="(tag, id) in info.hash_tag" :key="id"> #{{ tag }} </small>
       </div>
-      <slot name="button"></slot>
-      <slot name="slider"></slot>
+    </div>
+
+    <div class="row">
+      <div class="project-content padd-15">
+        <div class="project-title">
+          <h3>{{ $t('project-info.description') }}:</h3>
+          <p>
+            {{
+              info.description[$store.getters['i18n/getLanguage']] === ''
+                ? $t('project-info.description-empty')
+                : info.description[$store.getters['i18n/getLanguage']]
+            }}
+          </p>
+        </div>
+        <div class="project-tech-title">
+          <h3>{{ $t('project-info.info') }}:</h3>
+
+          <ul>
+            <li v-for="(dataProject, id) in info.info" :key="id">
+              <strong>
+                {{ dataProject.title[$store.getters['i18n/getLanguage']] }}
+              </strong>
+              -
+              <a
+                v-if="dataProject.description.indexOf('https') !== -1"
+                :href="dataProject.description"
+                target="_blank"
+              >
+                {{ dataProject.title[$store.getters['i18n/getLanguage']] }}
+              </a>
+              <template v-else>
+                {{ dataProject.description }}
+              </template>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+  <slot name="button"></slot>
+  <slot name="slider"></slot>
 </template>
 
 <script>
@@ -63,8 +62,6 @@ export default {
 
     onMounted(() => {
       store.commit('setStyleHeightCard', card.value)
-
-      // console.log('card.value', card.value)
     })
 
     return {
